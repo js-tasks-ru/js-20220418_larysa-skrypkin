d3eff598241dc5066989e298eed6ad520b2c0257
Larysa Skrypkina <larysa.skrypkin@gmail.com>
2022-05-09 00:25:40 +0300

    task 4

--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -31,14 +31,13 @@ export default class ColumnChart {
   getTemplate() {
     this.linkClass = this.link ? "column-chart__link" : "link-disabled";
     this.number = this.label === 'sales' ? `$${this.value}` : `${this.value}`;
-    this.container = this.data.length > 0 ? "" : `column-chart_loading`;
+    this.loading = this.data.length > 0 ? "" : "column-chart_loading";
  
     return `
-      <div class="column-chart" style="--chart-height: ${chartHeight}">
+      <div class="column-chart ${this.loading}" style="--chart-height: ${chartHeight}">
       <div class="column-chart__title">Total ${this.label}
       <a class=${this.linkClass}>View all</a>
       </div>
-      <div class=${this.container}>
            <div class="column-chart__container">
         <div data-element="header" class="column-chart__header">${this.number}</div>
         <div data-element="body" class="column-chart__chart">
@@ -46,7 +45,6 @@ export default class ColumnChart {
         </div>
       </div>
       </div>
-      <div>
      `;
   }
 
